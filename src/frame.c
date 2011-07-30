af38bc1abb4cd969f0853b85118cc5597bc2acff
Sam Bleckley <diiq.diiq@gmail.com>
2011-07-30 16:45:33 -0500

    More scraping and planing.

---
+++ src/frame.c
@@ -0,0 +1,117 @@
+#include "oyster.h"
+#include "machine.h"
+
+frame *make_frame(frame * below,
+                  table * scope,
+                  table * scope_to_be,
+                  table * scope_below, oyster * instruction, int flag)
+{
+    frame *ret = NEW(frame);
+
+    ret->below = below;
+    incref(ret->below);
+
+    ret->scope = scope;
+    incref(ret->scope);
+
+    ret->scope_to_be = scope_to_be;
+    incref(ret->scope_to_be);
+
+    ret->scope_below = scope_below;
+    incref(scope_below);
+
+    ret->instruction = instruction;
+    incref(instruction);
+
+    ret->flag = flag;
+
+    return ret;
+}
+
+void push_new_instruction(machine * m, oyster * instruction, int flag)
+{
+    frame *t = m->current_frame;
+    m->current_frame = make_frame(t,
+                                  m->now->scope,
+                                  m->now->scope_to_be,
+                                  m->now->scope_below, instruction, flag);
+    incref(m->current_frame);
+    decref(t);
+}
+
+void push_instruction_list(machine * m,
+                           oyster * ins,
+                           table * scope, table * scope_below)
+{
+    incref(ins);
+
+    frame *top = NULL;
+    frame **cur = &top;
+    while (!nilp(ins)) {
+        (*cur) = make_frame(NULL,
+                            scope, NULL, scope_below, car(ins), EVALUATE);
+        incref(*cur);
+
+        cur = &((*cur)->below);
+
+        oyster *ins2 = cdr(ins);
+        incref(ins2);
+        decref(ins);
+        ins = ins2;
+    }
+
+    decref(ins);
+    (*cur) = m->current_frame;
+    m->current_frame = top;
+}
+
+
+//-------------------------- Printing ----------------------------//
+
+void print_stack_trace(machine * m)
+{
+    printf("Now: ");
+    frame_print(m->now, 1);
+
+    frame *f = m->current_frame;
+    while (f) {
+        frame_print(f, 1);
+        f = f->below;
+    }
+    if (m->accumulator) {
+        printf("accum: ");
+        oyster_print(m->accumulator);
+    }
+    printf
+        ("\n--- --- --- --- --- --- --- --- --- --- --- --- --- --- ---\n\n\n\n");
+}
+
+void frame_print(frame * i, int print_scope)
+{
+    char *flags[] = { 
+        "ASTERPEND_CONTINUE",
+        "ATPEND_CONTINUE",
+        "ARGUMENT",
+        "ELIPSIS_ARGUMENT",
+        "ELIPSIS_ASTERPEND_CONTINUE",
+        "ELIPSIS_ATPEND_CONTINUE",
+        "EVALUATE",
+        "CONTINUE",
+        "APPLY_FUNCTION",
+        "PREPARE_ARGUMENTS",
+        "PAUSE",
+        "HANDLE_SIGNALS"
+    };
+
+    printf(" --> ");
+    printf("%s, ", flags[i->flag]);
+    if (i->instruction)
+        oyster_print(i->instruction);
+    if (print_scope) {
+        printf("\n Scope:\n");
+        table_print(i->scope);
+        printf("Upcoming scope:\n");
+        table_print(i->scope_to_be);
+    }
+    printf("\n");
+}
